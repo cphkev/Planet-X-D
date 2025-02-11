5084b3557f1c9be2f6b1c9cb9fdf3affa4d80016
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2025-02-11 10:56:00 +0100

    fred coin invest now

---
+++ Assets/Fred/Coin/CoinScript/CoinCollected.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CoinCollected : MonoBehaviour
+{
+    [SerializeField] AudioClip collectSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+            Destroy(gameObject);
+        }
+    }
+}
